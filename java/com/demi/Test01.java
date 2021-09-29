4bd5c197ec21d4b9bdd8d747559b5336dcb5edf2
xiangben <xbnine@163.com>
2021-09-29 13:22:39 +0800

    test01 master

---
+++ java/com/demi/Test01.java
@@ -0,0 +1,18 @@
+package demi;
+
+/**
+ * @author create at 2021/9/29 13:21
+ * @author xb
+ * @program dapaixiangmu
+ * @description: Test01
+ */
+public class Test01 {
+
+
+    /**
+     *  v.1
+     */
+    public void test01(){
+        System.out.println("test01");
+    }
+}
